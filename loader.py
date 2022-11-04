760f060201e3d20aff7a95a375a3cf50f32dce7d
Saeed Balquizi <66793847+bleubyte@users.noreply.github.com>
2022-11-04 19:03:02 -0400

    Updated loader.py
    Modified the loader to import datasets from yml files.

--- loader.py
+++ loader.py
@@ -1,19 +1,18 @@
 from elasticsearch import Elasticsearch
 
-import csv
+#import csv
+import yaml
+
 
 es = Elasticsearch(hosts=["http://127.0.0.1:9200"])
 
 print(f"Connected to ElasticSearch cluster `{es.info().body['cluster_name']}`")
 
-with open("./Car details v3.csv", "r") as f:
-    reader = csv.reader(f)
-
-    for i, line in enumerate(reader):
+with open('farc.yml', 'r') as file:
+    for doc in yaml.safe_load_all(file):
         document = {
-            "name": line[0],
-            "engine": line[9],
-            "year": line[1],
-            "price": line[2],
-        }
-        es.index(index="cars", document=document)
+                    "title": doc['title'],
+                    "description": doc['description'],
+                }
+        es.index(index="findings", document=document)
+
